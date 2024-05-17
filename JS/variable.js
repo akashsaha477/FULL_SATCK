58baf5c4f89d1d822f1822ead64214a080350150
Akash Saha <akashsaha477@gmail.com>
2024-05-18 04:06:20 +0600

    loops

--- JS/variable.js
+++ JS/variable.js
@@ -2,4 +2,11 @@ let Myname = "Akash Saha";
 let age = 21;
 let isMarried = false;
 
-console.log("My name is" + Myname + "My age is" + age + "My marital status is" + isMarried);
+console.log("My name is" + " " +Myname + " My age is " + age + " My marital status is " + isMarried + ".");
+
+if (isMarried==true){
+    console.log(Myname + " is married.");
+}
+else{
+    console.log(Myname + " is not married.");
+}
