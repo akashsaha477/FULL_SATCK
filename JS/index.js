1af7e3197d6118baef84fd04762df7e1718f9ce8
Akash Saha <akashsaha477@gmail.com>
2024-06-01 23:03:06 +0800

    practice.js

---
+++ JS/index.js
@@ -0,0 +1,16 @@
+function square(n){
+    return n*n;
+}
+
+function cube(n){
+    return n*n*n;
+}
+
+function Akash12(a,b,fntofunc1,fntofunc2){
+    //let p=fntofunc1(a);
+    //let q=fntofunc2(b);
+    return p+q; 
+
+}
+
+let y =console.log(Akash12(2,3,square,cube));
---
+++ JS/readfile.js
@@ -0,0 +1,4 @@
+var fs=require('fs')
+fs.readFile("a.txt","utf-8",function(err,data){
+    console.log(data);
+})
